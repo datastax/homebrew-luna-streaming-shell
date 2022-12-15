712bed89804e3f769ad37f0491173f7d86974054
Nicolò Boschi <boschi1997@gmail.com>
2022-12-15 15:50:07 +0100

    Update pulsar-shell.rb

--- pulsar-shell.rb
+++ pulsar-shell.rb
@@ -1,8 +1,8 @@
 class PulsarShell < Formula
     desc "Datastax Luna Streaming Shell for managing Apache Pulsar clusters"
     homepage "https://docs.datastax.com/en/luna-streaming/docs/index.html"
-    url "https://github.com/datastax/pulsar/releases/download/ls210_2.7/lunastreaming-shell-2.10.2.7-bin.tar.gz"
-    sha256 "f6a0c7b6c07a775a5ecce07df210119ee3a5dc3529cbcb28bdca7797e84ecf95"
+    url "https://github.com/datastax/pulsar/releases/download/ls210_2.9/lunastreaming-shell-2.10.2.9-bin.tar.gz"
+    sha256 "0efaba533064d57f02d763f9da08e7e7fb8f6136525af6519233d6279717333a"
     license "Apache-2.0"
   
     depends_on "openjdk@11"
