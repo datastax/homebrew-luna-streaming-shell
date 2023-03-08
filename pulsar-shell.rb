848da9bbe0d0513ace98c529524283859db56d8c
Nicolò Boschi <boschi1997@gmail.com>
2023-03-08 09:45:32 +0100

    Fix LS 2.10 3.4 sha

--- pulsar-shell.rb
+++ pulsar-shell.rb
@@ -2,7 +2,7 @@ class PulsarShell < Formula
     desc "Datastax Luna Streaming Shell for managing Apache Pulsar clusters"
     homepage "https://docs.datastax.com/en/luna-streaming/docs/index.html"
     url "https://github.com/datastax/pulsar/releases/download/ls210_3.4/lunastreaming-shell-2.10.3.4-bin.tar.gz"
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "43623d62c80f4c9155411013ec8401dbfec5ee0300e416a72706d01879cc6c6f"
     license "Apache-2.0"
   
     depends_on "openjdk@11"
