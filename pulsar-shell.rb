94a981e2d465996980d3f2cd7e5a204d4e366647
mukesh-ctds <ext-mukesh.kumar@datastax.com>
2024-03-07 15:56:49 +0530

    Update get-pulsar-shell & brew package to 3.1.3.0

--- pulsar-shell.rb
+++ pulsar-shell.rb
@@ -1,11 +1,11 @@
 class PulsarShell < Formula
     desc "Datastax Luna Streaming Shell for managing Apache Pulsar clusters"
     homepage "https://docs.datastax.com/en/luna-streaming/docs/index.html"
-    url " https://github.com/datastax/pulsar/releases/download/ls210_4.2/lunastreaming-shell-2.10.4.2-bin.tar.gz"
-    sha256 "254a412ab3a7c722e37ebc8f86d59949fa2e1ed40b13cf2d32c3963657cbbc60"
+    url " https://github.com/datastax/pulsar/releases/download/ls31_3.0/lunastreaming-shell-3.1.3.0-bin.tar.gz"
+    sha256 "cc2c69be4d2fcb6f8da3ed811c5352b4ce412afd25aa63738a3e0ac0a888112b"
     license "Apache-2.0"
   
-    depends_on "openjdk@11"
+    depends_on "openjdk@17"
   
     def install
       libexec.install Dir["*"]
