e16b0a5e4411e6f322537da8bf8a21faad922496
nikhil-ctds <151718832+nikhil-ctds@users.noreply.github.com>
2024-11-05 16:18:36 +0530

    Update pulsar-shell.rb

--- pulsar-shell.rb
+++ pulsar-shell.rb
@@ -1,8 +1,8 @@
 class PulsarShell < Formula
     desc "Datastax Luna Streaming Shell for managing Apache Pulsar clusters"
     homepage "https://docs.datastax.com/en/luna-streaming/docs/index.html"
-    url "https://github.com/datastax/pulsar/releases/download/ls31_4.7/lunastreaming-shell-3.1.4.7-bin.tar.gz"
-    sha256 "57b82d8fa8e5333a1d6ffaa388792b2c3dbcc60d933f4207834a6f06bbb12f07"
+    url "https://github.com/datastax/pulsar/releases/download/ls31_4.9/lunastreaming-shell-3.1.4.9-bin.tar.gz"
+    sha256 "3731d1b318effabaca1c4ccf4028fe84f3dbb070a0fc958253bf81f44b0a37f0"
     license "Apache-2.0"
   
     depends_on "openjdk@17"
